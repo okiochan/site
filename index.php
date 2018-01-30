6b23e6d8e198089b675bd189f2b80799c1c903f8
okiochan <maryalx@mail.ru>
2018-01-30 19:40:53 +0300

    q

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <!-- Animate.css -->
-	<link rel="stylesheet" href="style/css/animate.css">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="style/css/icomoon.css">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="style/css/bootstrap.css">
-	<!-- Owl Carousel -->
-	<link rel="stylesheet" href="style/css/owl.carousel.min.css">
-	<link rel="stylesheet" href="style/css/owl.theme.default.min.css">
-	<!-- Theme style  -->
-	<link rel="stylesheet" href="style/css/style.css">
-    <link rel="stylesheet" href="style/css/style_main.css">
-
-	<!-- Modernizr JS -->
-	<script src="style/js/modernizr-2.6.2.min.js"></script>
+   <link rel="stylesheet" href="style/css/style_main.css">
 </head>
 <body>
 
@@ -23,62 +9,69 @@
   <h1>Here may be text</h1>
 </div>
 
-<div class="clearfix">
-  <div class="menu column">
+<div class="left column">
     <h1>First</h1>
-    <?php
-        function GetImages($dir) {
-            $files = scandir($dir);
-            foreach ($files as $file) {
-                if($file == '.' || $file == "..")continue;
-                $result[] = $dir . "/" . $file;
+    <div class="gallery">
+        <?php
+            function GetImages($dir) {
+                $files = scandir($dir);
+                foreach ($files as $file) {
+                    if($file == '.' || $file == "..")continue;
+                    $result[] = $dir . "/" . $file;
+                }
+                return $result;
             }
-            return $result;
-        }
-        
-        function DisplayImages($dir) {
-            $links = GetImages($dir);
-            $format= ' <a href="portfolio1.php">
-                        <img src="%s">
-                        </a> ';
-            foreach ($links as $link) {
-              printf($format, htmlspecialchars($link));
+            
+            function DisplayImages($dir) {
+                $links = GetImages($dir);
+                $format= ' <a href="portfolio1.php">
+                            <img src="%s">
+                            </a> ';
+                foreach ($links as $link) {
+                  printf($format, htmlspecialchars($link));
+                }
             }
-        }
-        
-        $galleryname = "Sergey/main";
-        if (!empty($galleryname)) DisplayImages($galleryname);
-    ?>
-  </div>
+            
+            $galleryname = "Sergey/main";
+            if (!empty($galleryname)) DisplayImages($galleryname);
+        ?>
+    </div>
+</div>
 
-  <div class="column content">
+<div class="column right">
     <h1>Second</h1>
-    
-    <?php
-        function DisplayImagesOther($dir) {
-            $links = GetImages($dir);
-            $format= ' <a href="portfolio2.php">
-                        <img src="%s">
-                        </a> ';
-            foreach ($links as $link) {
-              printf($format, htmlspecialchars($link));
+    <div class="gallery">
+        <?php
+            function DisplayImagesOther($dir) {
+                $links = GetImages($dir);
+                $format= ' <a href="portfolio2.php">
+                            <img src="%s">
+                            </a> ';
+                foreach ($links as $link) {
+                  printf($format, htmlspecialchars($link));
+                }
             }
-        }
-        
-        $var = 1;
-        if($var != 1) {
-            echo "Wow";
-        }
-        
-        $galleryname = "Other/main";
-        DisplayImagesOther($galleryname);
-    ?>
-  </div>
+            
+            $galleryname = "Other/main";
+            DisplayImagesOther($galleryname);
+        ?>
+    </div>
+</div>
+  
+<div class="clearfix"></div>
+
+
+<div class="left_footer column">
+    <p>write here mail1</p>
+    <p>write here hello world</p>
 </div>
 
-<div class="footer">
-  <p>write here something</p>
+<div class="column right_footer">
+    <p>write here mail2</p>
+    <p>write here hello world</p>
 </div>
+  
+<div class="clearfix"></div>
 
 </body>
 </html>
