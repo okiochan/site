9c625d62081f667aa7acb057788e671813693ad7
okiochan <maryalx@mail.ru>
2018-02-19 17:42:48 +0300

    Signed-off-by: okiochan <maryalx@mail.ru>

--- php/include.php
+++ php/include.php
@@ -2,11 +2,10 @@
 session_start();
 
 define("DB_COMMENTS_PATH", __DIR__ . "/comments.db");
-
+define("DB_USERS_PATH", __DIR__ . "/users.db");
 
 define("MIN_PASS_LEN", 4);
 
-require_once __DIR__ . "/Users.php";
 require_once __DIR__ . "/DB.php";
-// require_once __DIR__ . "/login_user.php";
-// require_once __DIR__ . "/register_user.php";
+require_once __DIR__ . "/Users.php";
+
